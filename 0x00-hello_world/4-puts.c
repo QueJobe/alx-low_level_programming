be412f0662d2371952140371d8653297996e78a0
Blessing <bqsithole22@live.com>
2023-03-17 05:26:38 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,10 +7,8 @@
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-	getchar();
 
-	return 0;
-
+	return (0);
 }

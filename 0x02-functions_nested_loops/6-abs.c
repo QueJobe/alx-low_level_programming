84a4cbf6f35b493071dead07b480349c7bde6730
Blessing <bqsithole22@live.com>
2023-03-22 04:49:52 -0700

    Printing absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - print absolute value of a number
+ * @n will pass the number
+ * Return: n the absoulte value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}

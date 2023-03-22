d67917140a95173b603c629e315210c8f8cd87f1
Blessing <bqsithole22@live.com>
2023-03-22 11:41:27 -0700

    Printing 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - printing the times table
+ * Return: returns the times table
+ */
+void times_table(void)
+{
+	int r, c, n;
+
+	for (r = 0; r <= 9; r++)
+	{
+		_putchar(48);
+		_putchar(',');
+		_putchar(' ');
+
+		for (c = 0; c <= 9; c++)
+		{
+			n = r * c;
+			if ((n / 10) > 0)
+			{
+				_putchar ((n / 10) + 48);
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((n % 10) + 48);
+
+			if (c < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}

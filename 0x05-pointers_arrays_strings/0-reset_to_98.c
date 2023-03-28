ffd135db11470c238c06f623d7e0b72a55c2e173
Blessing <bqsithole22@live.com>
2023-03-28 01:09:32 -0700

    98 battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function to reset a point to 98
+ *
+ *@n: to pass int value to function
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef main_h
+#define main_h
+
+/**
+ * main.h - contains prototypes used in program
+ *
+ * returns: nothing
+ */
+
+void reset_to_98(int *n);
+#endif

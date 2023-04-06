0ddcbe5b77aebce37509b22cfedf942840aead30
Blessing <bqsithole22@live.com>
2023-04-06 13:18:09 -0700

    function that returns the natural square root of a number

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,4 +9,6 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 int _putchar(char c);
 void print_diagsums(int *a, int size);
+int _sqrt_recursion(int n);
+int _sqrt(int num, int base);
 #endif

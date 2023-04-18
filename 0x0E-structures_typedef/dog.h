eb3f2b9fe5610d020aed63bd5dbc12fab88342a9
Blessing <bqsithole22@live.com>
2023-04-18 15:58:20 -0700

    header file for strut dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a new stuct of tpye dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: Owner if the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*Header file dog.h*/

8cad5dd473e368c9f1c53c5ae0e84b784c556c65
Blessing <bqsithole22@live.com>
2023-05-12 14:36:09 -0700

    copy a file to a file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,6 +8,10 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+#define BUF_SIZE 1024
+
+void p_error(const char *e_msg, const char *filename, int code);
+void close_file(int fd);
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);

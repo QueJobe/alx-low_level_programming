091d8ea8156fe87ebeab477bdcd46a547fc21cf2
Blessing <blessingsithole87@gmail.com>
2023-07-14 03:12:54 -0700

    coreecting errors

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,7 +24,7 @@ int append_text_to_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	fd = open(filename, O_RDONLY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 

925818f2245f135050f592b6d9326e97357b42ae
Blessing <blessingsithole87@gmail.com>
2023-07-14 03:09:24 -0700

    function to ammend file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - function that appends text at the end of a file
+ * @filename: name of the file
+ * @text_content: text to add to the file
+ * Return: 1 on success and -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int len = 0;
+	int n_write;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	fd = open(filename, O_RDONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	n_write = write(fd, text_content, len);
+	if (n_write == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
